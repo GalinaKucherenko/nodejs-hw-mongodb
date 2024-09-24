91216c54af7d012f6929478551c812eb0d09d85e
GalinaKucherenko <galina.mail.k.n@gmail.com>
2024-09-24 22:37:54 +0300

    auth

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,7 +13,7 @@ import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { isValidId } from "../middlewares/isValidId.js";
-import { authenticate } from '../middlewares/authenticate.js';
+import authenticate from '../middlewares/authenticate.js';
 import { checkRoles } from '../middlewares/checkRoles.js';
 import { ROLES } from '../constants/index.js';
 
