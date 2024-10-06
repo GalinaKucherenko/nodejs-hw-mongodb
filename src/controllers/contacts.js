038fab58cafd739b0e6ced2137f20630e4e3dd32
GalinaKucherenko <galina.mail.k.n@gmail.com>
2024-10-06 11:52:40 +0300

    email and images

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,6 +10,8 @@ import { parsePaginationParams } from "../utils/parsePaginationParams.js";
 import { parseSortParams } from "../utils/parseSortParams.js";
 import { parseFilterParams } from "../utils/parseFilterParams.js";
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
 
 export const getContactsController = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
@@ -100,7 +102,11 @@ export const patchContactController = async (req, res, next) => {
   const photo = req.file;
   let photoUrl;
   if (photo) {
-    photoUrl = await saveFileToUploadDir(photo);
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
   }
 
   const result = await updateContact(id, userId, { ...req.body, photo: photoUrl });
